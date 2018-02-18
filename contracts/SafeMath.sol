afa560c1b53cbafb44c3d962eb05d4cbd7991144
mpolci <mpolci@gmail.com>
2018-02-18 22:37:39 +0100

    Add a sample ico smart contract

--- contracts/SafeMath.sol
+++ contracts/SafeMath.sol
@@ -6,4 +6,18 @@ library SafeMath {
     assert(c>=a && c>=b);
     return c;
   }
+
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
 }
---
+++ contracts/SampleICO.sol
@@ -0,0 +1,65 @@
+pragma solidity ^0.4.19;
+
+import "./ICOEngineInterface.sol";
+import "./KYCBase.sol";
+import "./SafeMath.sol";
+import "zeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+
+// This is a basic ico example, do not use it in production.
+contract SampleICO is ICOEngineInterface, KYCBase {
+    using SafeMath for uint;
+
+    ERC20 public token;
+    address public wallet;
+
+    // from ICOEngineInterface
+    uint public price;
+
+    // from ICOEngineInterface
+    uint public startTime;
+
+    // from ICOEngineInterface
+    uint public endTime;
+
+    // from ICOEngineInterface
+    uint public totalTokens;
+
+    // from ICOEngineInterface
+    uint public remainingTokens;
+
+
+    /**
+     *  After you deployed the SampleICO contract, you have to call the ERC20
+     *  approve() method to the deployed contract address to assign tokens to
+     *  be sold by the ICO.
+     */
+    function SampleICO(address [] kycSigner, address _token, address _wallet, uint _startTime, uint _endTime, uint _price, uint _totalTokens)
+        public KYCBase(kycSigner)
+    {
+        token = ERC20(_token);
+        wallet = _wallet;
+        startTime = _startTime;
+        endTime = _endTime;
+        price = _price;
+        totalTokens = _totalTokens;
+        remainingTokens = _totalTokens;
+    }
+
+    // from KYCBase
+    function releaseTokensTo(address buyer) internal returns(bool) {
+        require(now >= startTime && now < endTime);
+        uint amount = msg.value.mul(price);
+        remainingTokens = remainingTokens.sub(amount);
+        return token.transfer(buyer, amount);
+    }
+
+    // from ICOEngineInterface
+    function started() public view returns(bool) {
+        return now >= startTime;
+    }
+
+    // from ICOEngineInterface
+    function ended() public view returns(bool) {
+        return now >= endTime;
+    }
+}
