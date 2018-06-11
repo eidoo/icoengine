f0f86b2a92a844b6a78dbde18db61ca4f501a209
aspeziale <aspeziale@digitalidentity.tech>
2018-06-11 16:59:14 +0200

    Update indentation

--- contracts/SafeMath.sol
+++ contracts/SafeMath.sol
@@ -1,23 +1,23 @@
 pragma solidity ^0.4.19;
 
 library SafeMath {
-  function add(uint a, uint b) internal pure returns (uint) {
-    uint c = a + b;
-    assert(c>=a && c>=b);
-    return c;
-  }
-
-  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-    if (a == 0) {
-      return 0;
+    function add(uint a, uint b) internal pure returns (uint) {
+        uint c = a + b;
+        assert(c>=a && c>=b);
+        return c;
     }
-    uint256 c = a * b;
-    assert(c / a == b);
-    return c;
-  }
 
-  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+        uint256 c = a * b;
+        assert(c / a == b);
+        return c;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
 }
